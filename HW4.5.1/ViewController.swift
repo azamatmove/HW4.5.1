1ff0f3ec228953b632a65b29256101c44e672cc0
Azamat <azamatmove@gmail.com>
2022-01-17 14:08:53 +0600

    names and numbers

--- HW4.5.1/ViewController.swift
+++ HW4.5.1/ViewController.swift
@@ -1,24 +1,35 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController, UITableViewDataSource{
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     
     private lazy var tableView: UITableView = {
         let view = UITableView()
         view.dataSource = self
+        view.delegate = self
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubviews()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addContact))
+        self.tableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.tableView.reloadData()
     }
     
     
-    private var friends = [
+    var friends = [
         "Aza","Aidai","Gera","Beka","Beishekan","Talgar","Sam","One"
     ]
+    
+    var numbers = ["0700900800","0700900800","0700900800","0700900800","0700900800","0700900800","0700900800","0700900800"]
     
     private func setupSubviews(){
         view.addSubview(tableView)
@@ -28,7 +39,7 @@ class ViewController: UIViewController, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 10
+        return UITableView.automaticDimension
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -40,10 +51,44 @@ class ViewController: UIViewController, UITableViewDataSource{
         
         let cell = UITableViewCell()
         
-        cell.textLabel?.text = friends[index]
+        cell.textLabel?.text = "\(friends[index])  \(numbers[index])"
         
         return cell
     }
     
     
+    @objc func addContact(){
+        let alert = UIAlertController(title: "Добавить контакт", message: "Введите данные", preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            field.placeholder = "Имя"
+            field.returnKeyType = .next
+            field.keyboardType = .namePhonePad
+        }
+        
+        alert.addTextField { field in
+            field.placeholder = "Номер"
+            field.returnKeyType = .next
+            field.keyboardType = .numberPad
+        }
+        
+        alert.addAction(UIAlertAction(title: "Отменить", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Добавить", style: .default, handler: { _ in
+            guard let fields = alert.textFields, fields.count == 2 else {
+                    return
+                }
+            let nameField = fields[0]
+            let numberField = fields[1]
+            
+            guard let name1 = nameField.text , !name1.isEmpty,
+                  let number1 = numberField.text, !number1.isEmpty else{
+                      print("Ошибка")
+                      return
+                  }
+            self.friends.append("\(name1)")
+            self.numbers.append("\(number1)")
+            self.tableView.reloadData()
+        }))
+        present(alert, animated: true)
+    }
 }
